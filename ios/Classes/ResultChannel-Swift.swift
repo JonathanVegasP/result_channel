66ed7d3ac598b3c20984dccca53a7b3707775e4d
Jonathan Vegas Peixoto <jonathan.v.peixoto@gmail.com>
2025-07-18 03:10:25 -0300

    feat: added util function to create a result native pointer

--- ios/Classes/ResultChannel-Swift.swift
+++ ios/Classes/ResultChannel-Swift.swift
@@ -5,42 +5,40 @@ public typealias ResultCallback = @convention(c) (UnsafeMutablePointer<ResultNat
 public final class ResultChannel {
     public static var serializer: Serializer = BinarySerializer()
     
+    @inline(__always)
+    public static func createResultNative(status: ResultChannelStatus, data: Any?) -> UnsafeMutablePointer<ResultNative>? {
+        let serialized = serializer.serialize(value: data)
+        let size = serialized.count
+        
+        let data = UnsafeMutableRawPointer.allocate(byteCount: size, alignment: MemoryLayout<UInt8>.alignment).assumingMemoryBound(to: UInt8.self)
+        
+        _ = serialized.withUnsafeBytes { buffer in
+            memcpy(data, buffer.baseAddress!, size)
+        }
+        
+        let ptr = UnsafeMutableRawPointer.allocate(byteCount: MemoryLayout<ResultNative>.stride, alignment: MemoryLayout<ResultNative>.alignment)
+                
+        let result = ptr.initializeMemory(as: ResultNative.self, to: ResultNative(status: status, data: data, size: Int32(size)))
+        
+        return result
+    }
+    
     private let resultCallback: ResultCallback
     
     public init(resultCallback: @escaping ResultCallback) {
         self.resultCallback = resultCallback
     }
     
+    @inline(__always)
     public func success(_ value: Any?) {
-        let serialized = Self.serializer.serialize(value: value)
-        let size = serialized.count
-        
-        let data = UnsafeMutableRawPointer.allocate(byteCount: size, alignment: MemoryLayout<UInt8>.alignment).assumingMemoryBound(to: UInt8.self)
-        
-        _ = serialized.withUnsafeBytes { buffer in
-            memcpy(data, buffer.baseAddress!, size)
-        }
-        
-        let ptr = UnsafeMutableRawPointer.allocate(byteCount: MemoryLayout<ResultNative>.stride, alignment: MemoryLayout<ResultNative>.alignment)
-                
-        let result = ptr.initializeMemory(as: ResultNative.self, to: ResultNative(status: ResultChannelStatusOk, data: data, size: Int32(size)))
+        let result = Self.createResultNative(status: ResultChannelStatusOk, data: value)
         
         resultCallback(result)
     }
     
+    @inline(__always)
     public func failure(_ value: Any?) {
-        let serialized = Self.serializer.serialize(value: value)
-        let size = serialized.count
-        
-        let data = UnsafeMutableRawPointer.allocate(byteCount: size, alignment: MemoryLayout<UInt8>.alignment).assumingMemoryBound(to: UInt8.self)
-        
-        _ = serialized.withUnsafeBytes { buffer in
-            memcpy(data, buffer.baseAddress!, size)
-        }
-        
-        let ptr = UnsafeMutableRawPointer.allocate(byteCount: MemoryLayout<ResultNative>.stride, alignment: MemoryLayout<ResultNative>.alignment)
-                
-        let result = ptr.initializeMemory(as: ResultNative.self, to: ResultNative(status: ResultChannelStatusError, data: data, size: Int32(size)))
+        let result = Self.createResultNative(status: ResultChannelStatusError, data: value)
         
         resultCallback(result)
     }
