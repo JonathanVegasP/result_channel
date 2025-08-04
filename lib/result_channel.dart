7b93ce0c2fb9395f19ef375dbdef9d3b0fab3438
Jonathan Vegas Peixoto <jonathan.v.peixoto@gmail.com>
2025-08-03 21:06:23 -0300

    fix: export result native

--- lib/result_channel.dart
+++ lib/result_channel.dart
@@ -2,6 +2,7 @@ export 'src/binary_serializer.dart';
 export 'src/reader.dart';
 export 'src/result_channel.dart';
 export 'src/result_dart.dart';
+export 'src/result_native.dart';
 export 'src/result_status.dart';
 export 'src/serializer.dart';
 export 'src/typedefs.dart';
